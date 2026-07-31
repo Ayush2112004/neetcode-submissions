22aaddabd716b207412c6d68256e7d5f1d3de3d4
Ayush Rathi <ayushrathi125@gmail.com>
2026-07-31 23:41:38 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n=s.size();
+        int l=0;
+        int r=0;
+        vector<int> hash(26);
+        int maxLen=0;
+        int maxFreq=0;
+        while(r<n){
+            hash[s[r]-'A']++;
+            maxFreq=max(maxFreq,hash[s[r]-'A']);
+            int window=r-l+1;
+            if(window-maxFreq>k){
+                hash[s[l]-'A']--;
+                l++;
+            }
+            window=r-l+1;
+            maxLen=max(maxLen,window);
+            r++;
+        }
+        return maxLen;
+
+    }
+};
