dcd344d54d66ef5912ff708649bb41827b7155b2
Ayush Rathi <ayushrathi125@gmail.com>
2026-08-15 11:46:21 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int target=n-1;
+        for(int i=n-1;i>=0;i--){
+            if(i+nums[i]>=target){
+                target=i;
+            }
+        }
+        if(target<=0){
+            return true;
+        }
+        return false;
+    }
+};
