cd0fa7ab817b3774f205653915194ec7a26bf6d4
Ayush Rathi <ayushrathi125@gmail.com>
2026-08-23 13:09:09 +0530

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n=s.size();
+        unordered_map<char,int> um;
+        for(int i=n-1;i>=0;i--){
+            if(um.find(s[i])!=um.end()){
+                continue;
+            }
+            um[s[i]]=i;//storing the last occurence so that minimum till that we can make thepartition
+        }
+        vector<int> ans;
+        int i=0;
+        while(i<n){
+            int start=i;
+            int end=um[s[i]];
+            for(int j=start;j<=end;j++){
+                if(um[s[j]]>end){
+                    end=um[s[j]];
+                }
+            }
+            ans.push_back(end-start+1);
+            i=end+1;
+        }
+        return ans;
+    }
+};
